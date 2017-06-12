df290db4504e9715b8acc3999a8e0fbd7cd99697
Vojtěch Spurný <vojtech.spurny@fel.cvut.cz>
2017-06-12 15:15:09 +0200

    added build test

---
+++ .test_build.sh
@@ -0,0 +1,17 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+USER_REPO_PATH=`pwd`
+MY_WORKSPACE=~/workspace
+
+rm -rf $MY_WORKSPACE
+mkdir -p $MY_WORKSPACE/src
+
+ln -s $USER_REPO_PATH $MY_WORKSPACE/src/user_repo
+cd $MY_WORKSPACE
+catkin init
+catkin config --extend /opt/ros/kinetic
+catkin config --profile default --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+catkin config --profile release --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+catkin profile set default
+catkin build
