b082644ab3ef5583a0179a69005780caaf1f4f9a
Robert Penicka <penicrob@fel.cvut.cz>
2018-08-14 20:46:59 +0200

    change test build to melodic

--- .test_build.sh
+++ .test_build.sh
@@ -10,7 +10,7 @@ mkdir -p $MY_WORKSPACE/src
 ln -s $USER_REPO_PATH $MY_WORKSPACE/src/user_repo
 cd $MY_WORKSPACE
 catkin init
-catkin config --extend /opt/ros/kinetic
+catkin config --extend /opt/ros/melodic
 catkin config --profile default --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 catkin config --profile release --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 catkin profile set default
