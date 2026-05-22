0a5161559f9a463bb656c6236c8b6b6b171c31ef
Tomas Baca <Tomas Baca>
2026-05-22 12:11:49 +0200

    updated flake

--- flake.nix
+++ flake.nix
@@ -1,66 +1,78 @@
 {
-  description = "Essential custom messages used by different parts of the mrs-uav-system";
-
   inputs = {
     nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
-    nixpkgs.follows = "nix-ros-overlay/nixpkgs";
+    nixpkgs.follows = "nix-ros-overlay/nixpkgs";  # IMPORTANT!!!
   };
+  outputs = { self, nix-ros-overlay, nixpkgs }:
 
-  outputs = { self, nixpkgs, nix-ros-overlay }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ nix-ros-overlay.overlays.default ];
-      };
+    nix-ros-overlay.inputs.flake-utils.lib.eachDefaultSystem (system:
 
-      # Using Jazzy, but you can swap this to humble if needed
-      ros = pkgs.rosPackages.jazzy;
-    in {
+      let
+
+        system = "x86_64-linux";
+
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ nix-ros-overlay.overlays.default ];
+        };
+
+        ros = pkgs.rosPackages.jazzy;
+
+      in {
 
       packages.${system}.default = ros.buildRosPackage {
+
         pname = "mrs_msgs";
         version = "2.0.0";
-        src = ./.;
-
+        src = "./.";
+        
         buildType = "ament_cmake";
-
+        
         # Maps to <buildtool_depend>
-        nativeBuildInputs = [
-          ros.ament-cmake
-          ros.rosidl-default-generators
+        nativeBuildInputs = [ 
+          ros.ament-cmake 
+          ros.rosidl-default-generators 
         ];
-
+        
         # Maps to <depend> and <exec_depend>
-        buildInputs = [
+        buildInputs = [ 
+          ros.ros-core
+          ros.ament-cmake 
+          ros.ament-cmake-core
           ros.builtin-interfaces
-          ros.geometry-msgs
-          ros.rosidl-default-runtime
           ros.sensor-msgs
-          ros.std-msgs
           ros.std-srvs
+          ros.std-msgs
+          ros.geometry-msgs
+          ros.python-cmake-module
         ];
       };
 
-      devShells.${system}.default = pkgs.mkShell {
-        packages = [
-          pkgs.colcon
-          (ros.buildEnv {
-            packages = [
-              ros.ros-core
-              # Tools needed to compile locally
-              ros.ament-cmake
-              ros.rosidl-default-generators
-              # Message dependencies needed locally
-              ros.builtin-interfaces
-              ros.geometry-msgs
-              ros.rosidl-default-runtime
-              ros.sensor-msgs
-              ros.std-msgs
-              ros.std-srvs
-            ];
-          })
-        ];
-      };
-    };
+        devShells.default = pkgs.mkShell {
+          name = "mrs_msgs";
+          packages = [
+            pkgs.colcon
+            # ... ot<depend><depend><depend><depend>her non-ROS packages
+            (ros.buildEnv {
+              paths = [
+                ros.ros-core
+                ros.ament-cmake 
+                ros.ament-cmake-core
+                ros.builtin-interfaces
+                ros.sensor-msgs
+                ros.std-srvs
+                ros.std-msgs
+                ros.geometry-msgs
+                ros.python-cmake-module
+              ];
+            })
+          ];
+        };
+
+      });
+
+  nixConfig = {
+    extra-substituters = [ "https://ros.cachix.org" ];
+    extra-trusted-public-keys = [ "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo=" ];
+  };
 }
